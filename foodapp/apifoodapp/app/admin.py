17d8331e90414437a006327c0caff915fc81358c
le trung <lequoctrunggg@gmail.com>
2025-02-03 10:04:24 +0700

    mung 6

--- foodapp/apifoodapp/app/admin.py
+++ foodapp/apifoodapp/app/admin.py
@@ -34,8 +34,16 @@ class UserAdmin(admin.ModelAdmin):
 
 
 class RestaurantAdmin(admin.ModelAdmin):
-    list_display = ['name', 'address', 'phone_number', 'star_rate', 'owner']
+    list_display = ['name', 'address', 'phone_number', 'star_rate', 'owner', 'confirmation_status']
     search_fields = ["name"]
+    list_filter = ['confirmation_status']
+    actions = ['approve_restaurants']
+
+    def approve_restaurants(self, request, queryset):
+        queryset.update(confirmation_status=True)
+        self.message_user(request, "Nhà hàng đã được phê duyệt!")
+
+    approve_restaurants.short_description = "Phê duyệt nhà hàng đã chọn"
 
 
 class CartAdmin(admin.ModelAdmin):
@@ -55,14 +63,14 @@ class MenuAdmin(admin.ModelAdmin):
 
 
 admin_site = FoodAppAdminSite('myfoodapp')
-admin_site.register(Food, FoodAdmin)
-admin_site.register(RestaurantCategory, RestaurantCategoryAdmin)
-admin_site.register(MainCategory, MainCategoryAdmin)
+# admin_site.register(Food, FoodAdmin)
+# admin_site.register(RestaurantCategory, RestaurantCategoryAdmin)
+# admin_site.register(MainCategory, MainCategoryAdmin)
 admin_site.register(User, UserAdmin)
 admin_site.register(Restaurant, RestaurantAdmin)
-admin_site.register(Cart, CartAdmin)
-admin_site.register(SubCart, SubCartAdmin)
-admin_site.register(SubCartItem, SubCartItemAdmin)
-admin_site.register(Menu, MenuAdmin)
-admin_site.register(Order, admin.ModelAdmin)
-admin_site.register(OrderDetail, admin.ModelAdmin)
+# admin_site.register(Cart, CartAdmin)
+# admin_site.register(SubCart, SubCartAdmin)
+# admin_site.register(SubCartItem, SubCartItemAdmin)
+# admin_site.register(Menu, MenuAdmin)
+# admin_site.register(Order, admin.ModelAdmin)
+# admin_site.register(OrderDetail, admin.ModelAdmin)
